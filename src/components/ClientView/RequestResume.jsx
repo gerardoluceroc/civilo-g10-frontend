3d272dd05cadc03386c171d36fa3e2c87a487d39
gerardoluceroc <gerardo.lucero.c@usach.cl>
2023-06-28 11:30:23 -0400

    Ajustes en diseño de RequestResume

--- src/components/ClientView/RequestResume.jsx
+++ src/components/ClientView/RequestResume.jsx
@@ -1,25 +1,61 @@
 import { CalendarMonth, CalendarMonthOutlined, Info } from "@mui/icons-material";
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components";
 import Modal from "../Modal";
 import ModalSellerInformation from "../ModalRequestDetails";
 import ModalRequestDetails from "../ModalRequestDetails";
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 const StyledDiv = styled.div`
     background-color:  #7fb3d5;
     border: solid;
     border-radius: 10px;
     display: flex;
+    //flex-direction: column;
     align-items: center;
-    width: 300px;
+    width: 16%;
     margin: 5px; //separador vertical entre este elementos y otros hermanos
     margin-left: 10px;//separador horizontal respecto al borde izquierdo
     padding: 7px;
+    //padding-left: -5px;
+
+    @media (max-width: 1572px){
+      width: 18%;
+    }
+
+    @media (max-width: 1390px){
+      width: 25%;
+    }
+
+    @media (max-width: 998px){
+      width: 32%;
+    }
+
+    @media (max-width: 804px){
+      width: 36%;
+    }
+
+    @media (max-width: 734px){
+      width: 39%;
+    }
 
     @media (max-width: 679px){
       margin: auto;
-      width: 70%;
+      width: 65%;
     }
+
+    @media (max-width: 400px){
+      margin: auto;
+      width: 75%;
+    }
+
+    @media (max-width: 436px) {
+    width: 85%;
+    
+  }
+
 `;
 
 
@@ -35,6 +71,7 @@ const Fecha = styled.h3`
   margin-top: 0; /* Ajusta el margen superior del texto para que esté alineado con el ícono */
   padding: 0%;
 `;
+
 
 const VerDetalles = styled.button`
     background-color:#2ea3f0 ;
@@ -73,22 +110,23 @@ const EstadoSolicitud = styled.div`
 
   font-size: 150%; /* Tamaño de fuente del texto */
   align-items: center; 
-  height: 30px; /* Altura del contenedor */
-  width: 50%; /* Ancho del contenedor */
+  height: 40px; /* Altura del contenedor */
+  width: 56%; /* Ancho del contenedor */
   display: flex; /* Establece el contenedor como un elemento flex */
   justify-content: center; /* Centra horizontalmente los elementos */
   padding: 0.35rem; /* Espacio interior alrededor del contenido */
 
-  @media (max-width: 678px) {
+
+  @media (max-width: 679px) {
     margin: auto;
+    margin-left: 10%;
     
   }
 
-  @media (max-width: 400px) {
-    width: 35%;
-    font-size: 100%;
-    
-  }
+
+  
+
+
 
 
   //color letra cancelado: #9e1919, color fondo: #f5b7b1;
@@ -105,30 +143,59 @@ export const RequestResume = ({fecha,estado, colorFondoTag, colorLetraTag, IdSol
 
 const nuevaFecha = new Date(fecha);
 const fechaFormatoES = nuevaFecha.toLocaleDateString('es-ES').replace(/\//g, '-');
-
+const nombreUsuario = requestDetails.user.name;
+const apellidoUsuario = requestDetails.user.surname;
 const [modalOpen, setModalOpen] = useState(false);
+const [userInfo, setUserInfo] = useState(null);
+//const [Acciones, setAcciones] = useState(null)
+const [tipoUsuarioLogueado, setTipoUsuarioLogueado] = useState(JSON.parse(sessionStorage.getItem('user')).role.accountType);
+
+
+
+//Si el tipo de usuario logueado cambia
+useEffect(() => {
+  let componenteUserInfo = null;
+  //let componenteAcciones = null;
+  let tipoUsuario = tipoUsuarioLogueado.toLowerCase();
+
+  //si el tipo de usuario logueado no es cliente
+  if(tipoUsuario !== "cliente"){
+    //se muestra el nombre y apellido del cliente en el componente RequestResume
+    componenteUserInfo = <Fecha>
+                              <PersonIcon/>
+                              {`${nombreUsuario} ${apellidoUsuario}`}
+                          </Fecha>;
+  }
+
+  setUserInfo(componenteUserInfo);
+
+}, [tipoUsuarioLogueado])
+
 
   const handleModalClose = () => {
     setModalOpen(false);
   };
 
   return (
+
     <StyledDiv>
+
           <InfoSolicitud>
               <Titulo>{`Solicitud #${IdSolicitud}`}</Titulo>
+              {userInfo}
               <Fecha>
                   <CalendarioIcono/>
                   {fechaFormatoES}
               </Fecha>
+
+              <ModalRequestDetails open={modalOpen} onClose={handleModalClose} requestDetails={requestDetails} />    
+
               <VerDetalles onClick={() => setModalOpen(true)}>
-                  <Info/>
-                  Ver Detalles
-              </VerDetalles>  
-              <ModalRequestDetails open={modalOpen} onClose={handleModalClose} requestDetails={requestDetails} />      
+                <Info/>
+                Ver Detalles
+              </VerDetalles>
           </InfoSolicitud>
           <EstadoSolicitud backgroundColor={colorFondoTag} color={colorLetraTag}>{estado}</EstadoSolicitud>
-
-
 
     </StyledDiv>
   )
